4d772e0c07eba50bbcdfdd14e344ebef1447be2a
Conrado Alexsander <conradoalexsander@hotmail.com>
2020-06-30 13:14:35 -0300

    Added Avatar upload, JWT security

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '897e3a1c9775ad9a924baa08a6a7b9a9',
+    expiresIn: '1d',
+  },
+};
--- src/database/migrations/1593428364892-CreateUsers.ts
+++ src/database/migrations/1593428364892-CreateUsers.ts
@@ -1,14 +1,11 @@
 import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+import { formatDistanceToNow } from 'date-fns';
 
-// nome da tabela: letra minuscula, uderline, plural
-// você só pode alterar uma migration se ela não foi enviada para o controle de versão
-
-export default class CreateAppointments1593094479845
-  implements MigrationInterface {
+export default class CreateUsers1593428364892 implements MigrationInterface {
   public async up(queryRunner: QueryRunner): Promise<void> {
     await queryRunner.createTable(
       new Table({
-        name: 'appointments',
+        name: 'users',
         columns: [
           {
             name: 'id',
@@ -18,12 +15,18 @@ export default class CreateAppointments1593094479845
             default: 'uuid_generate_v4()',
           },
           {
-            name: 'provider',
+            name: 'name',
             type: 'varchar',
+            isNullable: false,
           },
           {
-            name: 'date',
-            type: 'timestamp with time zone',
+            name: 'email',
+            type: 'varchar',
+            isUnique: true,
+          },
+          {
+            name: 'password',
+            type: 'varchar',
           },
           {
             name: 'created_at',
@@ -41,6 +44,6 @@ export default class CreateAppointments1593094479845
   }
 
   public async down(queryRunner: QueryRunner): Promise<void> {
-    await queryRunner.dropTable('appointments');
+    await queryRunner.dropTable('users');
   }
 }
