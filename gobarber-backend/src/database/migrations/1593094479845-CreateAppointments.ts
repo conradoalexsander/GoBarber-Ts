0109dea822e0ba999017c2a911bd10640226d918
Conrado Alexsander <conradoalexsander@hotmail.com>
2020-08-02 19:11:53 -0300

    Restructured application

---
+++ gobarber-backend/src/database/migrations/1593094479845-CreateAppointments.ts
@@ -0,0 +1,46 @@
+import { MigrationInterface, QueryRunner, Table } from 'typeorm';
+
+// nome da tabela: letra minuscula, uderline, plural
+// você só pode alterar uma migration se ela não foi enviada para o controle de versão
+
+export default class CreateAppointments1593094479845
+  implements MigrationInterface {
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.createTable(
+      new Table({
+        name: 'appointments',
+        columns: [
+          {
+            name: 'id',
+            type: 'uuid',
+            isPrimary: true,
+            generationStrategy: 'uuid',
+            default: 'uuid_generate_v4()',
+          },
+          {
+            name: 'provider',
+            type: 'varchar',
+          },
+          {
+            name: 'date',
+            type: 'timestamp with time zone',
+          },
+          {
+            name: 'created_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+          {
+            name: 'updated_at',
+            type: 'timestamp',
+            default: 'now()',
+          },
+        ],
+      }),
+    );
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.dropTable('appointments');
+  }
+}
