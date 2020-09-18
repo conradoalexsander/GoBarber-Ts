a440f5720a5bb8b60b18a68f292a9852ae39fc7d
Conrado Alexsander <conradoalexsander@hotmail.com>
2020-09-17 23:20:00 -0300

    Finished appointments routes

---
+++ gobarber-backend/src/modules/appointments/infra/http/controllers/ProviderDayAvailabilityController.ts
@@ -0,0 +1,25 @@
+import { Request, Response } from 'express';
+
+import { container } from 'tsyringe';
+
+import ListProviderDayService from '@modules/appointments/services/ListProviderDayAvailabilityService';
+
+export default class ProvidersController {
+  public async index(request: Request, response: Response): Promise<Response> {
+    const { month, year, day } = request.body;
+    const { provider_id } = request.params;
+
+    const listProviderDayAvailability = container.resolve(
+      ListProviderDayService,
+    );
+
+    const availability = await listProviderDayAvailability.execute({
+      provider_id,
+      month,
+      year,
+      day,
+    });
+
+    return response.json(availability);
+  }
+}
